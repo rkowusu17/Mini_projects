2a396eb093e1b1c55acb26c053c4252ad6ad2727
Reginald Owusu <reginaldmiles45@gmail.com>
2023-12-19 20:42:39 +0000

    Added train counter

---
+++ Calculator/Train counter/apps.js
@@ -0,0 +1,27 @@
+// document.getElementById("count-el").innerText = 5;
+let count = 0;
+let countEl = document.getElementById("count-el");
+function increment() {
+  count += 1;
+  countEl.textContent = count;
+}
+
+let saveEl = document.getElementById("save-el");
+function save() {
+  let countStr = count + " - ";
+  saveEl.textContent += countStr;
+  countEl.textContent = 0;
+  count = 0;
+}
+
+function restart() {
+  countEl.textContent = 0;
+  count = 0;
+  // saveEl.textContent =
+}
+
+let welcomeEl = document.getElementById("welcome-el");
+let username = "reggie";
+let greeting = " Welcome back Agent";
+welcomeEl.innerText = greeting + " " + username;
+welcomeEl.innerText += "👋";
